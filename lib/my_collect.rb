1c37805accb617899da2d8aa5865c05fb74abf9b
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-12 19:35:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,12 @@
 def my_collect(array)
-  collection =[]
   if array.length==0
     puts "No data"
   else
     i = 0
     while i < array.length
-  collection<< yield(array[i])
+   yield(array[i])
       i=i+1
 end
-collection
 end
 end
 
